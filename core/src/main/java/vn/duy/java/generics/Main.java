f003997390d178c988a465f1b97107cc1e65f28c
duyle2011 <duyphi2011@gmail.com>
2025-07-01 21:34:30 +0700

    generics

--- core/src/main/java/vn/duy/java/generics/Main.java
+++ core/src/main/java/vn/duy/java/generics/Main.java
@@ -1,35 +1,48 @@
 package vn.duy.java.generics;
 
-record BaseballPlayer(String name, int position) {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o instanceof BaseballPlayer baseballPlayer) {
-            return baseballPlayer.name().equalsIgnoreCase(name);
-        }
-        return false;
-    }
-
-    @Override
-    public int hashCode() {
-        return name().hashCode();
-    }
+interface Player {}
+record BaseballPlayer(String name, int position) implements Player {
 }
+
+record SoccerPlayer(String name, int position) implements Player {
+}
+
 public class Main {
     public static void main(String[] args) {
-        BaseballTeam team1 = new BaseballTeam("Team 1");
-        BaseballTeam team2 = new BaseballTeam("Team 2");
+        BaseballTeam baseballTeam1 = new BaseballTeam("Team 1");
+        BaseballTeam baseballTeam2 = new BaseballTeam("Team 2");
+        scoreResult(baseballTeam1, 3, baseballTeam2, 5);
 
+        SoccerTeam soccerTeam1 = new SoccerTeam("Team 1");
+        SoccerTeam soccerTeam2 = new SoccerTeam("Team 2");
+        scoreResult(soccerTeam1, 3, soccerTeam2, 5);
+
+        Team<SoccerPlayer> team1 = new Team<>("Team 1");
+        Team<BaseballPlayer> team2 = new Team<>("Team 2");
         scoreResult(team1, 3, team2, 5);
 
         BaseballPlayer player1 = new BaseballPlayer("Duy", 1);
         BaseballPlayer player2 = new BaseballPlayer("Duy", 2);
-        team1.addTeamMember(player1);
-        team1.addTeamMember(player2);
-        team1.listTeamMembers();
+        baseballTeam1.addTeamMember(player1);
+        baseballTeam1.addTeamMember(player2);
+        baseballTeam1.listTeamMembers();
     }
 
     public static void scoreResult(BaseballTeam team1, int t1Score, BaseballTeam team2, int t2Score) {
+        String message = team1.setScore(t1Score, t2Score);
+        team2.setScore(t2Score, t1Score);
+
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(SoccerTeam team1, int t1Score, SoccerTeam team2, int t2Score) {
+        String message = team1.setScore(t1Score, t2Score);
+        team2.setScore(t2Score, t1Score);
+
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(Team team1, int t1Score, Team team2, int t2Score) {
         String message = team1.setScore(t1Score, t2Score);
         team2.setScore(t2Score, t1Score);
 
---
+++ core/src/main/java/vn/duy/java/generics/Team.java
@@ -0,0 +1,53 @@
+package vn.duy.java.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team<T extends Player> {
+    private String name;
+    private List<T> teamMembers = new ArrayList<>();
+
+    private int totalWins = 0;
+
+    private int totalLosses = 0;
+
+    private int totalTies = 0;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public void addTeamMember(T player) {
+        if (!teamMembers.contains(player)) {
+            teamMembers.add(player);
+        }
+    }
+
+    public void listTeamMembers() {
+        System.out.println(name + " Roster: ");
+        System.out.println(teamMembers);
+    }
+
+    public int ranking() {
+        return (totalLosses * 2) + totalTies + 1;
+    }
+
+    public String setScore(int ourScore, int theirScore) {
+        String message = "lost to";
+        if (ourScore > theirScore) {
+            this.totalWins++;
+            message = "beat";
+        } else if (ourScore == theirScore) {
+            this.totalTies++;
+            message = "tied";
+        } else {
+            this.totalLosses++;
+        }
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Ranked " + ranking() + ")";
+    }
+}
